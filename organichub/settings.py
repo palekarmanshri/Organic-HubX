df0b89dbf7225dcf2ba7a0d6fd83727e6ee55fad
Manshri Palekar <palekarmanshri45@gmail.com>
2026-05-15 11:51:16 +0530

    fixed sqlite

--- organichub/settings.py
+++ organichub/settings.py
@@ -92,6 +92,9 @@ else:
             'PASSWORD': '',
             'HOST': 'localhost',
             'PORT': '3306',
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            }
         }
     }
 
